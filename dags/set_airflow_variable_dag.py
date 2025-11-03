fef1bf489497ae4e17d91ef1178a995479ec2c8e
Sai Revanth Gunji <gunji.revanth@astronomer.io>
2025-11-03 12:45:00 +0530

    schedule_interval issue fixed

--- dags/set_airflow_variable_dag.py
+++ dags/set_airflow_variable_dag.py
@@ -6,7 +6,6 @@ from datetime import datetime
 @dag(
     dag_id='set_airflow_variable_dag',
     start_date=datetime(2023, 1, 1),
-    schedule_interval=None,
     catchup=False,
     tags=['example', 'variables', 'python_operator']
 )
