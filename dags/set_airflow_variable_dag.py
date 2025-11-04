c71568b465c9313e5cddb29e0dbf607aa8a5aae8
Sai Revanth Gunji <gunji.revanth@astronomer.io>
2025-11-04 13:00:49 +0530

    get airflow dag added

--- dags/set_airflow_variable_dag.py
+++ dags/set_airflow_variable_dag.py
@@ -15,18 +15,18 @@ def get_airflow_variable_dag():
 
 
 
-    def fetch_and_log_variables() -> None:
-        """
-        Fetch all variables stored in AWS Secrets Manager and log their values.
-        """
-        # Retrieve all variables from AWS Secrets Manager using Airflow's Variable.get method
-        # Variables are prefixed with 'airflow/variables/' in AWS Secrets Manager
-        variables_prefix = "airflow"
-        all_variables: Dict[str, str] = Variable.get(variables_prefix, deserialize_json=True)
+    # def fetch_and_log_variables() -> None:
+    #     """
+    #     Fetch all variables stored in AWS Secrets Manager and log their values.
+    #     """
+    #     # Retrieve all variables from AWS Secrets Manager using Airflow's Variable.get method
+    #     # Variables are prefixed with 'airflow/variables/' in AWS Secrets Manager
+    #     variables_prefix = "airflow"
+    #     all_variables: Dict[str, str] = Variable.get(variables_prefix, deserialize_json=True)
         
-        # Log each variable and its value
-        for key, value in all_variables.items():
-            print(f"Variable Key: {key}, Variable Value: {value}")
+    #     # Log each variable and its value
+    #     for key, value in all_variables.items():
+    #         print(f"Variable Key: {key}, Variable Value: {value}")
 
 
     def get_variable():
@@ -35,11 +35,11 @@ def get_airflow_variable_dag():
         print(f"The value of the variable is: {variable_value}")
 
 
-    fetch_and_log_task = PythonOperator(
-        task_id='fetch_and_log_variables',
-        python_callable=fetch_and_log_variables, 
-        retries=1, 
-    )
+    # fetch_and_log_task = PythonOperator(
+    #     task_id='fetch_and_log_variables',
+    #     python_callable=fetch_and_log_variables, 
+    #     retries=1, 
+    # )
 
 
 
@@ -50,7 +50,7 @@ def get_airflow_variable_dag():
     )
 
 
-    fetch_and_log_task >> get_variable_task 
+    get_variable_task 
 
 
 get_airflow_variable_dag()
